antonioribeiro/symfony

commit 324ad4ccfd631313d1e040cd2683c27007e75cf2

Update PluralizationRules.php

src/Symfony/Component/Translation/PluralizationRules.php
@@ -115,6 +115,7 @@ class PluralizationRules
             case 'pap':
             case 'ps':
             case 'pt':
+            case 'xbr':
             case 'so':
             case 'sq':
             case 'sv':
@@ -135,7 +136,6 @@ class PluralizationRules
             case 'ln':
             case 'mg':
             case 'nso':
-            case 'xbr':
             case 'ti':
             case 'wa':
                 return (($number == 0) || ($number == 1)) ? 0 : 1;

commit 85b03dc6be32c0567d553ef1c36f6cf24ad6e9b2

In Brazilian Portuguese zero is singular
PluralizationRules was correct, but https://github.com/symfony/symfony/commit/324ad4ccfd631313d1e040cd2683c27007e75cf2 broke it.

Online references (in portuguese):

http://lgcooper.wordpress.com/2008/10/08/zero-e-plural-ou-singular/

http://g1.globo.com/platb/portugues/2007/04/

src/Symfony/Component/Translation/PluralizationRules.php
@@ -18,6 +18,7 @@ namespace Symfony\Component\Translation;
  */
 class PluralizationRules
 {
+    // @codeCoverageIgnoreStart
     private static $rules = array();
 
     /**
@@ -114,7 +115,6 @@ class PluralizationRules
             case 'pap':
             case 'ps':
             case 'pt':
-            case 'xbr':
             case 'so':
             case 'sq':
             case 'sv':
@@ -137,6 +137,7 @@ class PluralizationRules
             case 'nso':
             case 'ti':
             case 'wa':
+            case 'xbr':
                 return (($number == 0) || ($number == 1)) ? 0 : 1;
 
             case 'be':
@@ -211,4 +212,6 @@ class PluralizationRules
 
         self::$rules[$locale] = $rule;
     }
+
+    // @codeCoverageIgnoreEnd
 }